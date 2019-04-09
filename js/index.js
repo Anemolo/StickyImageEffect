ed44475fa9877eb3249cb965bd1096059426eb5c
Daniel Velasquez <anemoleiros@gmail.com>
2019-04-09 14:48:36 -0400

    local image and prebuild script

--- js/index.js
+++ js/index.js
@@ -1,42 +1,41 @@
-import { Showcase } from "./Showcase";
-import { Slides } from "./Slides";
+import {
+  Showcase
+} from "./Showcase";
+import {
+  Slides
+} from "./Slides";
 
+import image1 from '../images/image.jpg'
 const container = document.getElementById("app");
 
-const slidesData = [
-  {
+const slidesData = [{
     // https://unsplash.com/photos/2_Ip8Tfi8jM
     // No credit required
-    image:
-      "https://images.unsplash.com/photo-1487768047333-c8781e88b283?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80",
+    image: image1,
     title: "Creativity"
   },
   {
     // https://unsplash.com/photos/Y2G16TB2fws
     // No credit required
-    image:
-      "https://images.unsplash.com/photo-1519058497187-7167f17c6daf?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2048&q=100",
+    image: "https://images.unsplash.com/photo-1519058497187-7167f17c6daf?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2048&q=100",
     title: "Time"
   },
   {
     //https://unsplash.com/photos/6tx6K80_IJw
     // No credit required
-    image:
-      "https://images.unsplash.com/photo-1520883579495-5592ae324d5d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80",
+    image: "https://images.unsplash.com/photo-1520883579495-5592ae324d5d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80",
     title: "Home"
   },
   {
     // https://unsplash.com/photos/05Y_dRUcMaQ
     // No credit required
-    image:
-      "https://images.unsplash.com/photo-1514296138597-3cbbf6ca4239?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80",
+    image: "https://images.unsplash.com/photo-1514296138597-3cbbf6ca4239?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80",
     title: "Curiosity"
   },
   {
     // https://unsplash.com/photos/hAb_8YM2kJw
     // No credit required
-    image:
-      "https://images.unsplash.com/photo-1500336866626-5a47b3158a48?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80",
+    image: "https://images.unsplash.com/photo-1500336866626-5a47b3158a48?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80",
     title: "Order"
   }
 ];
@@ -49,24 +48,32 @@ const showcase = new Showcase(slidesData, {
   onIndexChange: index => {
     slides.onMove(index);
   },
-  onZoomOutStart: ({ activeIndex }) => {
+  onZoomOutStart: ({
+    activeIndex
+  }) => {
     slides.appear();
   },
-  onZoomOutFinish: ({ activeIndex }) => {},
-  onFullscreenStart: ({ activeIndex }) => {
+  onZoomOutFinish: ({
+    activeIndex
+  }) => {},
+  onFullscreenStart: ({
+    activeIndex
+  }) => {
     slides.disperse(activeIndex);
   },
-  onFullscreenFinish: ({ activeIndex }) => {}
+  onFullscreenFinish: ({
+    activeIndex
+  }) => {}
 });
 
 showcase.mount(container);
 slides.mount(container);
 showcase.render();
 
-window.addEventListener("resize", function() {
+window.addEventListener("resize", function () {
   showcase.onResize();
 });
 
-window.addEventListener("mousemove", function(ev) {
+window.addEventListener("mousemove", function (ev) {
   showcase.onMouseMove(ev);
 });
